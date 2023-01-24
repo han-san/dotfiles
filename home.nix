fa1b078b5afc4fd4d99c1dd4273b69fc10f875a9
Johan Sandred <johan.sandred@protonmail.com>
2023-01-24 01:01:13 +0100

    Add a vimrc.nix file

--- home.nix
+++ home.nix
@@ -31,9 +31,7 @@
       . /home/johan/.profile
       '';
 
-    file.".ideavimrc".text = ''
-      source ~/.config/nvim/init-home-manager.vim
-    '';
+    file.".ideavimrc".text = import ./vimrc.nix;
 
     sessionVariables = {
       LESSHISTFILE = "-";
@@ -274,39 +272,7 @@
     coc = {
       enable = true;
     };
-    extraConfig = ''
-      noremap n j
-      noremap e k
-      noremap i l
-      noremap k n
-      noremap l u
-      noremap j e
-      noremap u i
-
-      noremap N J
-      noremap E K
-      noremap I L
-      noremap K N
-      noremap L U
-      noremap J E
-      noremap U I
-
-      let mapleader = ","
-
-      set number
-      set path+=**
-      set softtabstop=4
-      set shiftwidth=4
-      set shiftround
-      set report=0
-      set ignorecase
-      set smartcase
-      set spell
-      set spelllang=en,sv
-      set backupdir-=.
-      set backup
-      set undofile
-    '';
+    extraConfig = import ./vimrc.nix;
   };
 
   services.dunst = {
---
+++ vimrc.nix
@@ -0,0 +1,33 @@
+''
+  noremap n j
+  noremap e k
+  noremap i l
+  noremap k n
+  noremap l u
+  noremap j e
+  noremap u i
+
+  noremap N J
+  noremap E K
+  noremap I L
+  noremap K N
+  noremap L U
+  noremap J E
+  noremap U I
+
+  let mapleader = ","
+
+  set number
+  set path+=**
+  set softtabstop=4
+  set shiftwidth=4
+  set shiftround
+  set report=0
+  set ignorecase
+  set smartcase
+  set spell
+  set spelllang=en,sv
+  set backupdir-=.
+  set backup
+  set undofile
+''
