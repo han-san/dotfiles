45ea65ec8c0a84c7057c2128d22a7e43d3623939
Johan Sandred <johan.sandred@protonmail.com>
2023-07-23 00:20:40 +0200

    Add tailscale addresses to syncthing config

--- configuration.nix
+++ configuration.nix
@@ -118,15 +118,19 @@
       user = "johan";
       dataDir = "/home/johan/Sync";
       openDefaultPorts = true;
-      devices = {
+      devices = let
+        tailscaleDomain = "tailf7aba.ts.net";
+      in {
         Phone = {
           id = "KYLVEZL-RUMAOH3-EN43MXV-QXSTIMT-ABMWQCB-A3SNTTJ-MUYJK7C-KYIFJQT";
         };
         Desktop = {
           id = "I4VJCRD-G2YEKD6-YEI3UYN-CP6KT3T-F25MBXL-WNQFSBQ-JESNMZO-JYF7NQZ";
+          addresses = [ "tcp://hansan-desktop.${tailscaleDomain}:22000" ];
         };
         Federer = {
           id = "YFXUNZH-6V2DILS-H2BKHKD-7C4EOY7-7745UPQ-Z6TYDLI-V32JYT2-PGFAEQJ";
+          addresses = [ "tcp://federer.${tailscaleDomain}:22000" ];
         };
       };
       folders = {
