b01de641f290e21e1ff348a42861fc2fb3c948b6
Johan Sandred <johan.sandred@protonmail.com>
2023-07-23 19:20:56 +0200

    Add bash using the imports attribute

--- home/home.nix
+++ home/home.nix
@@ -8,6 +8,7 @@
     ./kak/kakrc.nix
     ./neovim/neovim.nix
     ./firefox.nix
+    ./bash.nix
   ];
 
   # This isn't needed (or allowed) if home-manager.useGlobalPkgs is true.
@@ -259,38 +260,6 @@
     extensions = [
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
     ];
-  };
-
-  programs.bash = {
-    enable = true;
-    historyControl = [ "ignoredups" ];
-    shellAliases = {
-      cp = "cp -iv";
-      mv = "mv -iv";
-      mkdir = "mkdir -pv";
-      rmdir = "rmdir -pv";
-      rm = "rm -Iv";
-
-      make = "make -j16";
-      cmake = "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1";
-
-      #ls = "ls --color=auto -h --group-directories-first";
-      #ll = "ls -l";
-      #la = "ls -A";
-
-      grep = "rg";
-      fgrep = "fgrep --color=auto";
-      egrep = "egrep --color=auto";
-
-      diff = "diff --color=auto";
-
-      cxx17 = "c++ -std=c++17";
-      cxx20 = "c++ -std=c++20";
-
-      cat = "bat";
-
-      wrap = "fmt";
-    };
   };
 
   programs.vscode = {
