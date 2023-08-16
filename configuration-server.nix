55335e863050272f01583d6f9e08d3c8c258c692
Johan Sandred <johan.sandred@protonmail.com>
2023-08-16 23:56:10 +0200

    Change server's allowed ssh keys to ed25519

--- configuration-server.nix
+++ configuration-server.nix
@@ -45,8 +45,8 @@
       kakoune
     ];
     openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFEXDJBrK/nRUtN448qukWkkQa4eMfX7DX4Nf5quHGIVHQDM39qbwSOk/hbyglQjr5Oz+4zmo8U2zPV1IFQvTBTBIsfk9YdYpnqdADeIAwv2Oaq5w7qlCzMJtqSqMC8Dvb955SgbQSUWDfyddEpzB+Cqwdqh8ra3Hn+SJJMO8M8NcCZ4UkzEFh1D3VUOdDbF6GYxufcRunQqxAuss2l+vEN6howOpE3gerDtzVCLzrTMkt38BouoYZ9oByQRwIFvy2VVTrP/QCuhu5eFD0wN0wLNXQPCB31WJp+AAT5f/pY3QymWNEpsusKlnkCtW7JUypWhYbR7VkfFcd6ibZDO7b johan@DESKTOP-U1NK4GN"
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDdeu5Ygm/qa+Fl7nFxWbesl+nEA8Gl9YQwaRf4kvKzWvWxMs/jnD5VK18aP6w+Q/r8RYvL4BxzJl5Po54YXesJ3BkxuLuKuXPjiLnSmrCiBmHlHGITYtklOQctW/F/Gq+hIOo0jnlGzP7MnlMIbeQUX5xoyBuXgemq/jYjDosCNkB9uocijHAsPSXF5mliDwBR6Qd236x3Z4eQybJbFc3METBdvxmELLU6Cj5Py1W8vfZHRhv3hchFU2RcGVCrG6aSaGmagT0KuaxkPXi7G0p9JIEgq6Qv1hN0AG3lF1+wJ9cyS1uCbe1WGmph/OY1ujgHisMF8E4LN/+Q4A1Ec+pwoJYpKjqvClhoLtzCTGea0BvSPgp3LifNAxJJYG9ORxb1QrxwwJQX3LpouKuPta+E3ojPG4xWA+5uYutnoJGHIS1treVzRYK45RgTcNsIH7dU71MmQEpu0xAqNhZkC/ljdiMvv3HLm8qt2tUqi/Itaez39V9YcP4aIqs9FPSgDwc= johan@hansan-laptop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHog6/0hxWQiHsnbomKBn1nLK7smKYfHGk0YcfOVq4n johan@hansan-laptop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAsSoptyi4et13k8Fmngq211lsNyyF44yxj33a52BVXA johan@hansan-desktop"
     ];
   };
 
