4c2265f8336ead59545ed0ae1ddbaf5afbf55e7a
Johan Sandred <johan.sandred@protonmail.com>
2023-08-17 20:36:11 +0200

    Only allow wheel members to execute sudo

--- configuration-common.nix
+++ configuration-common.nix
@@ -74,6 +74,8 @@
     man.generateCaches = true;
   };
 
+  security.sudo.execWheelOnly = true;
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
