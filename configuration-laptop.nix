e44fd392214926e0da5ca008294290ea9252ae1b
Johan Sandred <johan.sandred@protonmail.com>
2024-03-26 21:18:48 +0100

    Fix bluetooth config for pipewire
    They changed how you configure it.

--- configuration-laptop.nix
+++ configuration-laptop.nix
@@ -63,16 +63,17 @@
     pulse.enable = true;
   };
 
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  # An extraLuaConfig option should be available soon. Check https://nixos.wiki/wiki/PipeWire.
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
       bluez_monitor.properties = {
         ["bluez5.enable-sbc-xq"] = true,
         ["bluez5.enable-msbc"] = true,
         ["bluez5.enable-hw-volume"] = true,
         ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
       }
-    '';
-  };
+    '')
+  ];
 
   hardware.opengl = {
     enable = true;
